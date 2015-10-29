45ac6ce67d6f04dee52e17a65ab04c039c8e2741
JustinPriday <jpriday@gmail.com>
2015-10-29 13:06:03 +0200

    Update based on Vlad's feedback from his review of project submission.

--- app/src/main/java/com/justinpriday/nanodegree/projectzero/MainActivity.java
+++ app/src/main/java/com/justinpriday/nanodegree/projectzero/MainActivity.java
@@ -9,66 +9,40 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button project1Button;
-    private Button project2Button;
-    private Button project3Button;
-    private Button project4Button;
-    private Button project5Button;
-    private Button project6Button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        project1Button = (Button)findViewById(R.id.project1Button);
-        project2Button = (Button)findViewById(R.id.project2Button);
-        project3Button = (Button)findViewById(R.id.project3Button);
-        project4Button = (Button)findViewById(R.id.project4Button);
-        project5Button = (Button)findViewById(R.id.project5Button);
-        project6Button = (Button)findViewById(R.id.project6Button);
-
-        project1Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    public void showToast(View V) {
+        switch(V.getId()) {
+            case R.id.project1Button:
                 Toast.makeText(getApplicationContext(),"This button will launch my Spotify Streamer App", Toast.LENGTH_SHORT).show();
-            }
-        });
+                break;
 
-        project2Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            case R.id.project2Button:
                 Toast.makeText(getApplicationContext(),"This button will launch my Scores App", Toast.LENGTH_SHORT).show();
-            }
-        });
+                break;
 
-        project3Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"This button will launch my Library App", Toast.LENGTH_SHORT).show();
-            }
-        });
+            case R.id.project3Button:
+                Toast.makeText(getApplicationContext(),"This button will launch my Library Streamer App", Toast.LENGTH_SHORT).show();
+                break;
 
-        project4Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"This button will launch my Build It Bigger App", Toast.LENGTH_SHORT).show();
-            }
-        });
+            case R.id.project4Button:
+                Toast.makeText(getApplicationContext(),"This button will launch my Build It Bigger Streamer App", Toast.LENGTH_SHORT).show();
+                break;
 
-        project5Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"This button will launch my XYZ Reader App", Toast.LENGTH_SHORT).show();
-            }
-        });
+            case R.id.project5Button:
+                Toast.makeText(getApplicationContext(),"This button will launch my XYZ Reader Streamer App", Toast.LENGTH_SHORT).show();
+                break;
 
-        project6Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"This button will launch my Capstone App", Toast.LENGTH_SHORT).show();
-            }
-        });
+            case R.id.project6Button:
+                Toast.makeText(getApplicationContext(),"This button will launch my Capstone Streamer App", Toast.LENGTH_SHORT).show();
+                break;
 
+
+        }
     }
 }
+
